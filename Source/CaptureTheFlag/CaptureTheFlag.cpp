44c22da1084facf1eef3fdcb891f406989ba2ce0
Hash_Buoy <harshad.asawale@gmail.com>
2026-07-06 15:20:37 +0530

    Initial Commit

---
+++ Source/CaptureTheFlag/CaptureTheFlag.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CaptureTheFlag.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CaptureTheFlag, "CaptureTheFlag" );
+
+DEFINE_LOG_CATEGORY(LogCaptureTheFlag)
